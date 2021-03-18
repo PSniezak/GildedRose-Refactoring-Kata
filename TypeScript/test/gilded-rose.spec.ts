b72823ff4dffe875c05a990cf2cd993306043500
PSniezak <paulsniezak@gmail.com>
2021-03-18 12:36:58 +0100

    test: add general system checks

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,12 +1,25 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
-describe('Gilded Rose', function () {
+describe('Gilded Rose', () => {
+    describe('General', () => {
+        it('should add a new item with default values', () => {
+            const gildedRose = new GildedRose([ new Item('Beer', 0, 0) ]);
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
-    });
+            const [item] = gildedRose.items;
+    
+            expect(item.name).to.equal('Beer');
+            expect(item.sellIn).to.equal(0);
+            expect(item.quality).to.equal(0);
+        });
 
+        it("should decrease an item's values after a day", () => {
+            const gildedRose = new GildedRose([ new Item('Apple', 1, 1) ]);
+
+            const [item] = gildedRose.updateQuality();
+
+            expect(item.sellIn).to.equal(0);
+            expect(item.quality).to.equal(0);
+        })
+    })
 });
